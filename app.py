b4d7a5d53d1cca36655f938eef29a98bf1d121cf
Gerard Lucas <gerard.lucas@conperio.com>
2023-12-04 21:53:59 -0600

    remove unneeded variables

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/', methods=['GET'])
 def index():
-    page_title = "IT-4320 Trip Reservation System"
     return render_template('index.html')
 
 @app.route('/admin', methods=['GET'])
 def admin():
-    page_title = "Administrator Login"
     return render_template('admin.html')
 
 @app.route('/reservations', methods=['GET'])
 def reservation():
-    page_title = "Reserve Your Seat"
     return render_template('reservations.html')
 
 # start the flask app
